86a04c85f74bd9bde662d4b8e1108413457b8175
nourheneBS <17166871+nourheneBS@users.noreply.github.com>
2023-01-15 16:46:59 +0100

    added result OBJECT Model

---
+++ src/main/java/fr/joul/cie/test/springtechnicaltest/entities/AvailableOffers.java
@@ -0,0 +1,19 @@
+package fr.joul.cie.test.springtechnicaltest.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+//Result OBJECT model
+public class AvailableOffers {
+    private String code;
+    private float discountValue;
+    private String endDate;
+
+    private List<OfferDetails> offerList;
+}
---
+++ src/main/java/fr/joul/cie/test/springtechnicaltest/entities/OfferDetails.java
@@ -0,0 +1,13 @@
+package fr.joul.cie.test.springtechnicaltest.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OfferDetails {
+    private String name;
+    private String type;
+}
